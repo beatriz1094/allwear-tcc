bd98303c07ba2f3c137e319c1ccb87abbcbed79b
catarina2060 <eucatarinapereiraa@gmail.com>
2025-09-23 15:56:27 -0300

    atts

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,33 +1,15 @@
 const express = require('express');
 const routes = express.Router();
 
-const Usuario = require('./controllers/usuario');
-const Login = require('./controllers/login');
-const Escolha = require('./controllers/escolha');
+const usuario = require('./controllers/usuario.js'); 
 
-const { validate } = require('./middlewares/auth');
-
-// Rotas públicas
 routes.get('/', (req, res) => {
-  return res.json({ titulo: 'all wear' });
+    res.json({ titulo: 'API DD respondendo' });
 });
 
-// Remova estas rotas duplicadas se não existirem nos controllers
- routes.post('/login', Login.login);
-routes.get('/login', Login.validaToken);
-
-routes.post('/usuario', Usuario.create);
-routes.get('/usuario', validate, Usuario.read);
-routes.get('/usuario/:id', validate, Usuario.readOne);
-routes.patch('/usuario/:id', validate, Usuario.update);
-routes.delete('/usuario/:id', validate, Usuario.remove);
-
-
-// Rotas protegidas (exemplo: Escolha)
-routes.post('/escolha', Escolha.create);
-routes.get('/escolha', Escolha.read);
-routes.get('/escolha/:id', Escolha.readOne);
-routes.patch('/escolha/:id', Escolha.update);
-routes.delete('/escolha/:id', Escolha.remove);
+routes.post('/usuarios', usuario.create); 
+routes.get('/usuarios', usuario.read); 
+routes.put('/usuarios/:id', usuario.update); 
+routes.delete('/usuarios/:id', usuario.deletar); 
 
 module.exports = routes;
